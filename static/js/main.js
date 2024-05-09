a32a80f20b9e4e668da1f255f792a50d2b8180ce
Silverster98 <1539168414@qq.com>
2024-05-10 00:43:19 +0800

    bulma

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,12 @@
 function clickme() {
-  document.getElementById("demo").innerHTML = "Hello World!";
+
+  if (document.getElementById("my-btn").classList.contains("is-primary")) {
+    document.getElementById("my-par").innerHTML = "Hello <strong>World</strong>!"
+    document.getElementById("my-btn").classList.remove("is-primary")
+    document.getElementById("my-btn").classList.add("is-danger")
+  } else if (document.getElementById("my-btn").classList.contains("is-danger")) {
+    document.getElementById("my-par").innerHTML = "My first website with <strong>Bulma</strong>!"
+    document.getElementById("my-btn").classList.remove("is-danger")
+    document.getElementById("my-btn").classList.add("is-primary")
+  }
 }
