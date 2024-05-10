d1894b3529fc3649bcfb37cd10daa546e1358178
Silverster98 <1539168414@qq.com>
2024-05-10 11:29:32 +0800

    carousel

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,13 @@
-function clickme() {
-
-  if (document.getElementById("my-btn").classList.contains("is-primary")) {
-    document.getElementById("my-par").innerHTML = "Hello <strong>World</strong>!"
-    document.getElementById("my-btn").classList.remove("is-primary")
-    document.getElementById("my-btn").classList.add("is-danger")
-  } else if (document.getElementById("my-btn").classList.contains("is-danger")) {
-    document.getElementById("my-par").innerHTML = "My first website with <strong>Bulma</strong>!"
-    document.getElementById("my-btn").classList.remove("is-danger")
-    document.getElementById("my-btn").classList.add("is-primary")
+$(document).ready(function() {
+  // Check for click events on the navbar burger icon
+  var options = {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    loop: true,
+    infinite: false,
+    autoplay: false,
+    autoplaySpeed: 100000,
   }
-}
+  // Initialize all div with carousel class
+  var carousels = bulmaCarousel.attach('.carousel', options);
+});
